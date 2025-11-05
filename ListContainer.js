66b3bab67fdc3e0533a7431ea3cc03c4ccf6a8a9
Tomomi Hobara <tomomihobra@hotmail.com>
2025-11-05 09:54:48 -0600

    Fix useEffect

--- ListContainer.js
+++ ListContainer.js
@@ -8,24 +8,23 @@ export default function ListContainer({apiEndpoint}) {
     const baseUrl = 'https://swapi.tech/api/';
 
     
-    async function fetchData() {
-        try {
-            const response = await fetch(baseUrl + apiEndpoint);
-            const results = await response.json();
-            setData(results.results || results.result); // Extract the results array. Films array is 'result' not 'results'
-            setLoading(false); // Set loading to false when done
-        } catch (err) {10
-            setError('Failed to fetch data: ' + err.message);
-            setLoading(false);
-        }
-    }
-    
     useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(baseUrl + apiEndpoint);
+                const results = await response.json();
+                setData(results.results || results.result); // Extract the results array. Films array is 'result' not 'results'
+                setLoading(false); // Set loading to false when done
+            } catch (err) {
+                setError('Failed to fetch data: ' + err.message);
+                setLoading(false);
+            }
+        }
+
         fetchData();}, [apiEndpoint]);
 
     return (
         <List data={data}
-        fetchData={fetchData}
         loading={loading}
         error={error}
         />
